e46948c499f3a4464dca3fe58d33290c5a94d215
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2024-11-18 12:05:59 +0100

    refactor: externalisation de la fonction ask_user_input

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
+operand1 = None
+operator = None
+operand2 = None
+
 def main():
-    # Get first operand from the user
-    operand1 = float(input("Enter the first operand: "))
 
-    # Get the operator from the user
-    operator = input("Enter an operator (+, -, *, /): ")
-
-    # Get second operand from the user
-    operand2 = float(input("Enter the second operand: "))
+    ask_user_input()
 
     # Perform the operation based on the operator
     if operator == '+':
@@ -27,6 +25,44 @@ def main():
     # Print the result
     print("Result:", result)
 
+    #calculate(operand1, operator, operand2)
+    #display_result()
+
+def ask_user_input():
+    # Get first operand from the user
+    global operand1
+    operand1 = float(input("Enter the first operand: "))
+
+    # Get the operator from the user
+    global operator
+    operator = input("Enter an operator (+, -, *, /): ")
+
+    # Get second operand from the user
+    global operand2
+    operand2 = float(input("Enter the second operand: "))
+
+def calculate():
+    # Perform the operation based on the operator
+    if operator == '+':
+        result = operand1 + operand2
+    elif operator == '-':
+        result = operand1 - operand2
+    elif operator == '*':
+        result = operand1 * operand2
+    elif operator == '/':
+        if operand2 == 0:
+            print("Error: Division by zero is undefined.")
+            return
+        result = operand1 / operand2
+    else:
+        print("Invalid operator.")
+        return
+    pass
+
+def display_result():
+    # Print the result
+    print("Result:", result)
+    pass
 
 # Call the main function to run the program
 main()
